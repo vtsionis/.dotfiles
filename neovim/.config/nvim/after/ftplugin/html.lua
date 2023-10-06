ac2965df9f04e8f8767b8f30055c7a922a91067e
Vasileios Tsionis <vtsionis@protonmail.com>
2023-10-06 22:37:46 +0200

    handle cursor between opening and closing tag

--- neovim/.config/nvim/after/ftplugin/html.lua
+++ neovim/.config/nvim/after/ftplugin/html.lua
@@ -10,3 +10,19 @@ for option, value in pairs(options) do
     vim.opt_local[option] = value
 end
 
+-- When the cursor is between an opening and a closing tag, move it to a new
+-- empty line while the closing tag is moved one line below, upon hitting Enter
+vim.keymap.set("i", "<CR>", function()
+    local current_line = vim.fn.getline(".")
+    local column = vim.fn.getpos(".")[3]
+
+    local before = string.sub(current_line, column - 1, column - 1)
+    local after = string.sub(current_line, column, column)
+
+    if before == ">" and after == "<" then
+        return "<CR><C-o>O"
+    end
+
+    return "<CR>"
+end, { expr = true })
+
