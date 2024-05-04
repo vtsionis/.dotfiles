e5422b687a077d982b630176fb0ff0d2bf561f8d
Vasileios Tsionis <vtsionis@protonmail.com>
2024-05-04 09:40:48 +0200

    Neovim - Move ftplugin files into the after directory

--- neovim/.config/nvim/ftplugin/go.lua
+++
@@ -1,3 +0,0 @@
--- go fmt doesn't expand tabs
--- in fact, it transforms spaces into a tab
-
--- neovim/.config/nvim/ftplugin/text.lua
+++
@@ -1,8 +0,0 @@
-local options = {
-    spell = false,
-}
-
-for option, value in pairs(options) do
-    vim.opt_local[option] = value
-end
-
