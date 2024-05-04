7c9ab08493a583c3472bd737383623e5ac910aee
Vasileios Tsionis <vtsionis@protonmail.com>
2024-05-04 10:09:03 +0200

    Neovim - Install nvim-ufo to handle folding

---
+++ neovim/.config/nvim/lua/plugins/nvim-ufo.lua
@@ -0,0 +1,75 @@
+local ftMap = {
+    vim = "indent",
+    python = "indent",
+}
+
+local handler = function(virtText, lnum, endLnum, width, truncate)
+    local newVirtText = {}
+    local suffix = (' 󰁂 %d '):format(endLnum - lnum)
+    local sufWidth = vim.fn.strdisplaywidth(suffix)
+    local targetWidth = width - sufWidth
+    local curWidth = 0
+    for _, chunk in ipairs(virtText) do
+        local chunkText = chunk[1]
+        local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+        if targetWidth > curWidth + chunkWidth then
+            table.insert(newVirtText, chunk)
+        else
+            chunkText = truncate(chunkText, targetWidth - curWidth)
+            local hlGroup = chunk[2]
+            table.insert(newVirtText, {chunkText, hlGroup})
+            chunkWidth = vim.fn.strdisplaywidth(chunkText)
+            -- str width returned from truncate() may less than 2nd argument, need padding
+            if curWidth + chunkWidth < targetWidth then
+                suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
+            end
+            break
+        end
+        curWidth = curWidth + chunkWidth
+    end
+    table.insert(newVirtText, {suffix, 'MoreMsg'})
+    return newVirtText
+end
+
+return {
+    "kevinhwang91/nvim-ufo",
+    dependencies = {
+        "kevinhwang91/promise-async",
+        {
+            "luukvbaal/statuscol.nvim",
+            config = function()
+                local builtin = require("statuscol.builtin")
+                require("statuscol").setup({
+                    relculright = true,
+                    segments = {
+                        { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+                        { text = { "%s" }, click = "v:lua.ScSa" },
+                        { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+                    },
+                })
+            end,
+        },
+    },
+    event = "BufReadPost",
+    opts = {
+        open_fold_hl_timeout = 150,
+
+        provider_selector = function(_, filetype)
+            return ftMap[filetype] or "lsp"
+        end,
+
+        fold_virt_text_handler = handler,
+    },
+    config = function(_, opts)
+        local ufo = require("ufo")
+
+        vim.keymap.set("n", "zR", ufo.openAllFolds)
+        vim.keymap.set("n", "zM", ufo.closeAllFolds)
+        vim.keymap.set("n", "zr", ufo.openFoldsExceptKinds)
+        vim.keymap.set("n", "zm", ufo.closeFoldsWith)
+        vim.keymap.set("n", "zK", ufo.peekFoldedLinesUnderCursor)
+
+        ufo.setup(opts)
+    end,
+}
+
