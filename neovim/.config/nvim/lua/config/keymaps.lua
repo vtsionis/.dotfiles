aa466dd8f90af30a499b3297995ee4aad949b11e
Vasileios Tsionis <vtsionis@protonmail.com>
2024-05-04 09:56:26 +0200

    Neovim - Use a setup method to load keymaps

--- neovim/.config/nvim/lua/config/keymaps.lua
+++ neovim/.config/nvim/lua/config/keymaps.lua
@@ -1,5 +1,7 @@
 -- See `:help vim.keymap.set()`
 
+local M = {}
+
 -- Utility function to create a mapping
 local function map(mode, lhs, rhs, options)
     options = options or {}
@@ -8,68 +10,87 @@ local function map(mode, lhs, rhs, options)
     vim.keymap.set(mode, lhs, rhs, options)
 end
 
--- Disable the space key, since it will be used as the leader
-map({ "n", "v" }, "<Space>", "<Nop>")
+M.setup = function()
+    -- Disable the space key, since it will be used as the leader
+    map({ "n", "v" }, "<Space>", "<Nop>")
 
--- Disable arrow keys in normal mode
-map("n", "<Left>", "<Nop>")
-map("n", "<Right>", "<Nop>")
-map("n", "<Up>", "<Nop>")
-map("n", "<Down>", "<Nop>")
+    -- Disable arrow keys in normal mode
+    map("n", "<Left>", "<Nop>")
+    map("n", "<Right>", "<Nop>")
+    map("n", "<Up>", "<Nop>")
+    map("n", "<Down>", "<Nop>")
 
--- Toggle list mode
-map("n", "<leader>l", ":set list!<CR>")
+    -- Better up/down with wrapped lines
+    map("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+    map("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 
--- Better up/down with wrapped lines
-map("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-map("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+    -- Add undo break points
+    map("i", ",", ",<C-g>u")
+    map("i", ".", ".<C-g>u")
+    map("i", "!", "!<C-g>u")
+    map("i", "?", "?<C-g>u")
+    map("i", ":", ":<C-g>u")
+    map("i", ";", ";<C-g>u")
 
--- Don't move the cursor when joining lines
-map("n", "J", "mzJ`z")
+    -- Toggle list mode
+    map("n", "<leader>l", ":set list!<CR>")
 
--- Center cursor while moving around
-map("n", "<C-d>", "<C-d>zz")
-map("n", "<C-u>", "<C-u>zz")
+    -- Don't move the cursor when joining lines
+    map("n", "J", "mzJ`z")
 
--- Center search results
-map("n", "n", "nzz")
-map("n", "N", "Nzz")
+    -- Center cursor while moving around
+    map("n", "<C-d>", "<C-d>zz")
+    map("n", "<C-u>", "<C-u>zz")
 
--- Cancel search highlighting with ESC
-map("n", "<Esc>", "<cmd>nohlsearch<CR>")
+    -- Center search results
+    map("n", "n", "nzz")
+    map("n", "N", "Nzz")
 
--- Move selected line / block of text
-map("n", "<M-j>", "<cmd>m .+1<CR>==")
-map("n", "<M-k>", "<cmd>m .-2<CR>==")
-map("v", "<M-j>", ":m '>+1<CR>gv=gv")
-map("v", "<M-k>", ":m '<-2<CR>gv=gv")
+    -- Cancel search highlighting with ESC
+    map("n", "<Esc>", "<cmd>nohlsearch<CR><Esc>")
 
--- Stay in indent mode
-map("v", "<", "<gv")
-map("v", ">", ">gv")
+    -- Move selected line / block of text
+    map("n", "<M-j>", "<cmd>m .+1<CR>==")
+    map("n", "<M-k>", "<cmd>m .-2<CR>==")
+    map("v", "<M-j>", ":m '>+1<CR>gv=gv")
+    map("v", "<M-k>", ":m '<-2<CR>gv=gv")
 
--- Paste over currently selected text without yanking it
-map("v", "p", [["_dP]])
+    -- Stay in indent mode
+    map("v", "<", "<gv")
+    map("v", ">", ">gv")
 
--- Yank and paste to system clipboard
-local register = (vim.loop.os_uname().sysname == "Linux") and "+" or "*"
-map({ "n", "v" }, "<leader>y", [["]] .. register .. [[y]])
-map({ "n", "v" }, "<leader>d", [["]] .. register .. [[d]])
-map({ "n", "v" }, "<leader>p", [["]] .. register .. [[p]])
-map({ "n", "v" }, "<leader>P", [["]] .. register .. [[P]])
+    -- Paste over currently selected text without yanking it
+    map("v", "p", [["_dP]])
 
--- Diagnostic keymaps
-map("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
-map("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
-map("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
-map("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+    -- Yank and paste to system clipboard
+    local register = (vim.loop.os_uname().sysname == "Linux") and "+" or "*"
+    map({ "n", "v" }, "<leader>y", [["]] .. register .. [[y]])
+    map({ "n", "v" }, "<leader>d", [["]] .. register .. [[d]])
+    map({ "n", "v" }, "<leader>p", [["]] .. register .. [[p]])
+    map({ "n", "v" }, "<leader>P", [["]] .. register .. [[P]])
 
--- Move to a window using the <ctrl> hjkl keys
-map("n", "<C-h>", "<C-w>h", { desc = "Move focus to left window" })
-map("n", "<C-l>", "<C-w>l", { desc = "Move focus to right window" })
-map("n", "<C-j>", "<C-w>j", { desc = "Move focus to lower window" })
-map("n", "<C-k>", "<C-w>k", { desc = "Move focus to upper window" })
+    -- Diagnostic keymaps
+    map("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
+    map("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
+    map("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
+    map("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
--- Buffer interactions
-map("n", "<leader>w", "<cmd>w<CR>", { desc = "Save File" })
+    -- Move to a window using the <ctrl> hjkl keys
+    map("n", "<C-h>", "<C-w>h")
+    map("n", "<C-l>", "<C-w>l")
+    map("n", "<C-j>", "<C-w>j")
+    map("n", "<C-k>", "<C-w>k")
+
+    -- Cycle through open buffers
+    map("n", "[b", ":bprevious<CR>")
+    map("n", "]b", ":bnext<CR>")
+
+    -- Buffer interactions
+    map("n", "<leader>w", "<cmd>w<CR>", { desc = "Save File" })
+end
+
+-- Specify keymaps for plugins
+M.lsp = {}
+
+return M;
 
