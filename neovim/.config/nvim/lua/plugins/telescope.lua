203f0d01a90584a3518db4ab88bc85e152f21136
Vasileios Tsionis <vtsionis@protonmail.com>
2024-05-24 23:45:54 +0200

    Neovim - Update Telescope config

--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -1,3 +1,5 @@
+local keys = require("config.keymaps").telescope
+
 return {
     "nvim-telescope/telescope.nvim",
     branch = "0.1.x",
@@ -8,17 +10,39 @@ return {
 
         -- Suggested
         "BurntSushi/ripgrep",
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+        "nvim-telescope/telescope-ui-select.nvim",
 
         -- Optional
         "nvim-tree/nvim-web-devicons",
     },
+    keys = keys or {},
     config = function()
+        local telescope = require("telescope")
         local actions = require("telescope.actions")
         local action_layout = require("telescope.actions.layout")
+        local themes = require("telescope.themes")
 
-        require("telescope").setup({
+        local extensions = {
+            fzf = {
+                fuzzy = true,
+                override_generic_sorter = true,
+                override_file_sorter = true,
+                case_mode = "respect_case",
+            },
+            ["ui-select"] = {
+                themes.get_dropdown(),
+            },
+        }
+
+        telescope.setup({
             defaults = {
+                wrap_results = true,
+                path_display = { "smart" },
                 mappings = {
+                    n = {
+                        ["<M-p>"] = action_layout.toggle_preview,
+                    },
                     i = {
                         ["<Esc>"] = actions.close,
                         ["<C-u>"] = false,
@@ -36,7 +60,13 @@ return {
                     },
                 },
             },
+
+            extensions = extensions,
         })
+
+        for extension in pairs(extensions) do
+            telescope.load_extension(extension)
+        end
     end,
 }
 
