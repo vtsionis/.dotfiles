cd55cc0df9e3d6c3e178747f120f921d56f17eb4
Vasileios Tsionis <vtsionis@protonmail.com>
2024-05-24 23:41:14 +0200

    Neovim - Migrate to using extra modules for Treesitter

--- neovim/.config/nvim/lua/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -6,11 +6,41 @@ return {
         "BufNewFile",
     },
     dependencies = {
-        -- Place here any plugins that depend on Treesitter
-        "HiPhish/nvim-ts-rainbow2",
-        "windwp/nvim-ts-autotag",
-        "JoosepAlviste/nvim-ts-context-commentstring",
-        "nvim-treesitter/playground",
+        -- Extra modules
+        "HiPhish/rainbow-delimiters.nvim",
+        {
+            "windwp/nvim-ts-autotag",
+            ft = {
+                "astro",
+                "glimmer",
+                "handlebars",
+                "hbs",
+                "html",
+                "javascript",
+                "javascriptreact",
+                "jsx",
+                "php",
+                "rescript",
+                "svelte",
+                "tsx",
+                "typescript",
+                "typescriptreact",
+                "vue",
+                "xml",
+            },
+            config = true,
+        },
+        {
+            "JoosepAlviste/nvim-ts-context-commentstring",
+            lazy = false,
+            config = function()
+                vim.g.skip_ts_context_commentstring_module = true
+
+                require("ts_context_commentstring").setup({
+                    enable_autocmd = false,
+                })
+            end,
+        },
     },
     cmd = {
         "TSInstall",
@@ -54,39 +84,6 @@ return {
         -- see `:h nvim-treesitter-modules`
         -- Available plugins
         highlight = {
-            enable = true,
-        },
-
-        -- Extra modules (listed in dependencies)
-        rainbow = {
-            enable = true,
-        },
-        autotag = {
-            enable = true,
-            filetypes = {
-                "html",
-                "javascript",
-                "typescript",
-                "javascriptreact",
-                "typescriptreact",
-                "svelte",
-                "vue",
-                "tsx",
-                "jsx",
-                "rescript",
-                "xml",
-                "php",
-                "astro",
-                "glimmer",
-                "handlebars",
-                "hbs",
-            },
-        },
-        ts_context_commentstring = {
-            enable = true,
-            enable_autocmd = false,
-        },
-        playground = {
             enable = true,
         },
     },
