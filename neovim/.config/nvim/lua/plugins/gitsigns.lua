3ea19e45b28a7c895a7753ded25aa22911178d28
Vasileios Tsionis <vtsionis@protonmail.com>
2024-05-24 23:44:58 +0200

    Neovim - Get icons from config

--- neovim/.config/nvim/lua/plugins/gitsigns.lua
+++ neovim/.config/nvim/lua/plugins/gitsigns.lua
@@ -1,13 +1,15 @@
+local git_icons = require("utils.icons").git
+
 return {
     "lewis6991/gitsigns.nvim",
     opts = {
         signs = {
-            add = { text = "+" },
-            change = { text = "~" },
-            delete = { text = "_" },
-            topdelete = { text = "‾" },
-            changedelete = { text = "~" },
-            untracked = { text = "┆" },
+            add = { text = git_icons.add },
+            change = { text = git_icons.change },
+            delete = { text = git_icons.delete },
+            topdelete = { text = git_icons.todelete },
+            changedelete = { text = git_icons.changedelete },
+            untracked = { text = git_icons.untracked },
         },
     },
     lazy = false,
