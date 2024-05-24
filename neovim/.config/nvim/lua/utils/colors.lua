3bb16382bcf5a42808f6376b5acff7f52e4db149
Vasileios Tsionis <vtsionis@protonmail.com>
2024-05-24 23:42:52 +0200

    Neovim - Create a utils directory

---
+++ neovim/.config/nvim/lua/utils/colors.lua
@@ -0,0 +1,34 @@
+return {
+    black = "#191A21",
+    grey = "#44475A",
+    red = "#FF5555",
+    pink = "#FF79C6",
+    orange = "#FFB86C",
+    yellow = "#F1FA8C",
+    green = "#50FA7B",
+    blue = "#6F12F4",
+    steelblue = "#6272A4",
+    purple = "#BD93F9",
+    magenta = "#FF79C6",
+    cyan = "#8BE9FD",
+    white = "#F8F8F2",
+
+    dark_black = "#000000",
+    dark_grey = "#282A36",
+
+    bright_black = "#6272A4",
+    bright_grey = "#5f6A8E",
+    bright_red = "#FF6E6E",
+    bright_pink = "#FFA1D7",
+    bright_orange = "#FFD185",
+    bright_yellow = "#FFFFA5",
+    bright_green = "#69FF94",
+    bright_blue = "#D6ACFF",
+    bright_purple = "#D0B3FA",
+    bright_magenta = "#FF92DF",
+    bright_cyan = "#A4FFFF",
+    bright_white = "#FFFFFF",
+
+    neon_blue = "#04D9FF",
+}
+
