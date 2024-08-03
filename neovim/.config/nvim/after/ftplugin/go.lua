c459ff3652c28655a4af78d783eed29e48530cbe
Vasileios Tsionis <vtsionis@protonmail.com>
2024-08-03 08:26:22 +0200

    Noevim - Make sure hard tabs is used for Go files

--- neovim/.config/nvim/after/ftplugin/go.lua
+++ neovim/.config/nvim/after/ftplugin/go.lua
@@ -1,8 +1,9 @@
--- go fmt doesn't expand tabs
--- in fact, it transforms spaces into a tab
 local options = {
+   -- Make sure a hard tab is used
    expandtab = false,
+   shiftwidth = 0,
    smarttab = false,
+   softtabstop = 0,
 }
 
 for option, value in pairs(options) do
